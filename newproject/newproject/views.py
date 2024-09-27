92f35ea03980f67114216265be59334481a59edd
Faisal amin <faisalamin011@gmail.com>
2024-09-27 17:58:30 +0500

    Project 01 of Django

---
+++ newproject/newproject/views.py
@@ -0,0 +1,29 @@
+# views.py
+# I have created this file - Harry
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+    # return HttpResponse("Home")
+
+def removepunc(request):
+    #Get the text
+    djtext = request.GET.get('text', 'default')
+    print(djtext)
+    #Analyze the text
+    return HttpResponse("remove punc")
+
+def capfirst(request):
+    return HttpResponse("Capatilize first")
+
+def newlineremove(request):
+    return HttpResponse("New Line Remove")
+
+def spaceremove(request):
+    return HttpResponse("Space Remove")
+
+def charcount(request):
+    return HttpResponse("Charactor count")
